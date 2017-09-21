b087d832e51be4c3730207ff27782065c52d624e
yui <yuikita21@gmail.com>
2017-09-21 20:02:40 +0900

    get references

--- ref_network.py
+++ ref_network.py
@@ -1,3 +1,5 @@
+import os
+
 def get_references(text):
   r = text.find('REFERENCE')
   return text[r:]
@@ -6,22 +8,22 @@ def get_head(text):
   return text[:500]
 
 def validate(point):
-  p = point > 40
+  p = point > 5
   return p
+
+def skipwords(tar, skips):
+  return list(filter(lambda x:not x in skips, tar))
 
 def is_referenced(from_text, to_text):
   refs_words = get_references(from_text).split(' ')
+  refs_words = skipwords(refs_words, SW)
   tops_words = get_head(to_text).split(' ')
-  print(refs_words)
-  print("vs")
-  print(tops_words)
-  print()
-  point = 0
+  tops_words = skipwords(tops_words, SW)
+  rst = []
   for rw in refs_words:
     if rw in tops_words:
-      point += 1
-      print(rw)
-  return validate(point)
+      rst.append(rw)
+  return validate(len(rst))
 
 def get_text(path):
   with open(path) as f:
@@ -30,5 +32,37 @@ def get_text(path):
 to_text = get_text('text/101.txt')
 from_text = get_text('text/103.txt')
 
-print(is_referenced(from_text, to_text))
+def test_all_pattern(text_paths):
+  for i in range(len(text_paths)):
+    for k in range(len(text_paths))[i+1:]:
+      to_text_path = text_paths[i]
+      from_text_path = text_paths[k]
+      to_text = get_text(to_text_path)
+      from_text = get_text(from_text_path)
+      flag = is_referenced(from_text, to_text)
+      if flag:
+        print(from_text_path, ',', to_text_path)
+
+def pathlize(a):
+  return '/'.join(a)
+
+def get_all_paths(r, d=list()):
+  if r.find('.txt') != -1:
+    d += [r]
+    return
+  else:
+    for f in os.listdir(r):
+      if not f == '.DS_Store':
+        get_all_paths(pathlize([r, f]), d)
+  return d
+
+def only_txt(paths):
+  return list(filter(lambda x:x.find('txt') != -1, paths))
+
+SW = list(set(get_text('skipwords.txt').split('\n')))
+paths = get_all_paths('referencing')
+text_paths = only_txt(paths)
+test_all_pattern(text_paths)
+
+
 
